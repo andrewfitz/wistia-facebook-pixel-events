d90b095b62647c6b8d59cd17138f65b9e3358050
Andrew Fitzgerald <andrewfitz@gmail.com>
2019-04-18 21:03:50 -0400

    refactored code

--- wfbpe.js
+++ wfbpe.js
@@ -1,3 +1,7 @@
+fnk = function fnk(pcw) {
+	fbq('trackCustom', 'WistiaWatched', {percent: pcw, url: window.location.href, id: video.hashedId()});
+}
+
 window._wq = window._wq || [];
 _wq.push({
   id: '_all',
@@ -5,17 +9,17 @@ _wq.push({
     const secondsWatchedDisplay = document.getElementById('seconds_watched_display');
 	    video.bind('percentwatchedchanged', (percent, lastPercent) => {
 		  if (percent >= .01 && lastPercent < .01) {
-		    fbq('trackCustom', 'WistiaWatched', {percent: '1%', url: window.location.href, id: video.hashedId()});
+		    fnk('1%');
 		  } else if (percent >= .25 && lastPercent < .25) {
-		    fbq('trackCustom', 'WistiaWatched', {percent: '25%', url: window.location.href, id: video.hashedId()});
+		    fnk('25%');
 		  } else if (percent >= .50 && lastPercent < .50) {
-		    fbq('trackCustom', 'WistiaWatched', {percent: '50%', url: window.location.href, id: video.hashedId()});
+		    fnk('50%');
 		  } else if (percent >= .75 && lastPercent < .75) {
-		    fbq('trackCustom', 'WistiaWatched', {percent: '75%', url: window.location.href, id: video.hashedId()});
+		    fnk('75%');
 		  } else if (percent >= .95 && lastPercent < .95) {
-		    fbq('trackCustom', 'WistiaWatched', {percent: '95%', url: window.location.href, id: video.hashedId()});
+		    fnk('95%');
 		  } else if (percent >= 1 && lastPercent < 1) {
-		    fbq('trackCustom', 'WistiaWatched', {percent: '100%', url: window.location.href, id: video.hashedId()});
+		    fnk('100%');
 		  } 
     });
   },
